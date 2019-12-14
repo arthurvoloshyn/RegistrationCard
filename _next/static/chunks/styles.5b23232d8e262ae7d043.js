6e86006f28993f83e8bfe2445f50e0241b9c5c36
Artur <grandfaraon93@gmail.com>
2019-12-14 18:48:27 +0200

    Updates

--- _next/static/chunks/styles.5b23232d8e262ae7d043.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"6CI0":function(n,o,c){},JkUq:function(n,o,c){},VAPu:function(n,o,c){},oaNO:function(n,o,c){},s6kC:function(n,o,c){}}]);
---
+++ precache-manifest.67794a30d6bd70af8e08168399895a5a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c13d928704a41b9f8826f1fdab195c4",
+    "url": "/RegistrationCard/index.html"
+  },
+  {
+    "revision": "c2d064aa2bb6323d6b9c",
+    "url": "/RegistrationCard/static/css/2.d89ac54c.chunk.css"
+  },
+  {
+    "revision": "cb988667aec829e76abc",
+    "url": "/RegistrationCard/static/css/main.177d853b.chunk.css"
+  },
+  {
+    "revision": "c2d064aa2bb6323d6b9c",
+    "url": "/RegistrationCard/static/js/2.fd16ab2c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RegistrationCard/static/js/2.fd16ab2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb988667aec829e76abc",
+    "url": "/RegistrationCard/static/js/main.054f152b.chunk.js"
+  },
+  {
+    "revision": "eea18bfa13248cc1dc50",
+    "url": "/RegistrationCard/static/js/runtime-main.7b1e5a50.js"
+  },
+  {
+    "revision": "4303ff4194b86c7edc456ce6bf568527",
+    "url": "/RegistrationCard/static/media/libre-franklin-v4-latin-regular.4303ff41.woff2"
+  },
+  {
+    "revision": "59e5a4c43b99a456a399d48b10f6f5b3",
+    "url": "/RegistrationCard/static/media/libre-franklin-v4-latin-regular.59e5a4c4.svg"
+  },
+  {
+    "revision": "8a9c346da8378dc1862a5704b69ebf39",
+    "url": "/RegistrationCard/static/media/libre-franklin-v4-latin-regular.8a9c346d.woff"
+  },
+  {
+    "revision": "8e88ac945fbab4b87be1c03e84a3fbb2",
+    "url": "/RegistrationCard/static/media/libre-franklin-v4-latin-regular.8e88ac94.eot"
+  },
+  {
+    "revision": "eeea5982fdcd942af149380b6206b9f0",
+    "url": "/RegistrationCard/static/media/libre-franklin-v4-latin-regular.eeea5982.ttf"
+  },
+  {
+    "revision": "4dc7de3347f48884a7fb27e0ba6b8f6b",
+    "url": "/RegistrationCard/static/media/vt323-v11-latin-regular.4dc7de33.woff2"
+  },
+  {
+    "revision": "6a98b6bbf5f79b15e459956553250fa9",
+    "url": "/RegistrationCard/static/media/vt323-v11-latin-regular.6a98b6bb.ttf"
+  },
+  {
+    "revision": "a9329a516de470610aee05dbc31265e3",
+    "url": "/RegistrationCard/static/media/vt323-v11-latin-regular.a9329a51.eot"
+  },
+  {
+    "revision": "d037f8cb75b5764ae757659ae551cc62",
+    "url": "/RegistrationCard/static/media/vt323-v11-latin-regular.d037f8cb.woff"
+  },
+  {
+    "revision": "e91ab05faa8d30bbe01397d6cf56cf48",
+    "url": "/RegistrationCard/static/media/vt323-v11-latin-regular.e91ab05f.svg"
+  }
+]);
