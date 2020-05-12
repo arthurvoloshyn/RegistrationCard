11e86688ae3ae450301d5275945dbf3a4d7b3903
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-05-12 20:06:37 +0300

    Update config of next

--- next.config.js
+++ next.config.js
@@ -7,35 +7,36 @@ const withFonts = require('next-fonts');
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 
-const offlineConfig = {
-  transformManifest: manifest => ['/'].concat(manifest),
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-};
-
 module.exports = withPlugins([
   [withSourceMaps],
   [withFonts],
   [withImages],
   [withCSS],
   [withSass],
-  [withOffline, offlineConfig],
+  [
+    withOffline,
+    {
+      transformManifest: manifest => ['/'].concat(manifest),
+      workboxOpts: {
+        swDest: 'static/service-worker.js',
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'https-calls',
+              networkTimeoutSeconds: 15,
+              expiration: {
+                maxEntries: 150,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    },
+  ],
 ]);
